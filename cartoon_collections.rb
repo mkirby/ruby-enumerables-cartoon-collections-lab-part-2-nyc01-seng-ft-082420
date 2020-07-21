15f090262ce5bb5584ed769a3c6c7134ff3fa910
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-21 15:01:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.find do |element|
-    valid_calls.include(element)
+    if valid_calls.include(element)
       element
     end
   end
